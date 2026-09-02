8b588079164abeb4bf67814a41ffb52ce292d88f
Srini09git <srinimohand@gmail.com>
2026-09-02 21:33:48 +0530

    new class day 4

---
+++ Class/Day4.js
@@ -0,0 +1,85 @@
+// conditions
+
+//if 
+
+// let age = 17
+
+// if(age >= 18) { 
+//     console.log("You are an adult") 
+// }
+
+
+// if, else
+
+    // let age = 18
+
+    // if(18 >= 18){ 
+        
+    //     console.log("your are an adult")
+    // } 
+    // else{
+    //     console.log("you are not an adult")
+    // }
+
+// if , else, else if
+
+// let mark = 34;
+
+// if (mark >=35){
+//     console.log(" pass")
+// }
+// else {
+//     console.log("fail")
+// }
+
+//
+
+// let mark = 34
+
+// if(mark >= 90 ){
+//     console.log("Rank : A")
+// }
+// else if(mark >= 75){
+//     console.log("Rank : B")
+// }
+// else if(mark >=50){
+//     console.log ("Rank : C")
+// }
+// else if(mark >= 35){
+//     console.log("Rank : D")
+    
+// }
+// else{
+//     console.log("Rank : Fail")
+//     console.log("thambi thapu")
+// }
+
+
+// === strict equality
+
+// let age = 20
+
+// if(age !== "20"){ //false !false => true
+//     console.log("Crt")
+// }
+
+
+
+//logical AND
+
+// let age = 20
+// let hasID = false
+
+// if(age >=18 && hasID === true){
+//     console.log("you can enter")
+// }
+
+
+//logical OR
+
+let Student = false
+let Admin = false;
+
+if( Admin || Student){
+    console.log("Enter the office room")
+}
